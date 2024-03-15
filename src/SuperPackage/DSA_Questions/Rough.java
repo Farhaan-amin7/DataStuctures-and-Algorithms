3960414ab8249970340a2b23ddb3bc8ff8eea622
Farhaan Amin Mir <farhaanamin2020@gmail.com>
2024-03-15 22:14:04 +0530

    One file update

--- src/SuperPackage/DSA_Questions/Rough.java
+++ src/SuperPackage/DSA_Questions/Rough.java
@@ -1,8 +1,19 @@
 package SuperPackage.DSA_Questions;
 
+import java.util.*;
+
 public class Rough {
     public static void main(String[] args) {
-       int ans = (int) (Math.random() * 5 - 6);
-       int [] arr = new int[]
+     int [] arr = {2,6,5,2,4,4,5};
+        System.out.println(ans(arr));
+
     }
+    static int ans(int [] arr){
+        int uni = 0;
+        for( int n : arr){
+            uni= uni ^ n;
+        }
+        return uni;
+    }
+
 }
