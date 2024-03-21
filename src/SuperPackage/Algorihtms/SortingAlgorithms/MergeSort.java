e71aff20dc783ac56efbcc8709d4df017f330316
Farhaan Amin Mir <farhaanamin2020@gmail.com>
2024-03-21 22:36:08 +0530

    Merge Sort add

---
+++ src/SuperPackage/Algorihtms/SortingAlgorithms/MergeSort.java
@@ -0,0 +1,106 @@
+package SuperPackage.Algorihtms.SortingAlgorithms;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(arr));
+         MergesortInPlace(arr,0,arr.length);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static int[] Mergesort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+        int mid = arr.length / 2;
+
+        int[] left = Mergesort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = Mergesort(Arrays.copyOfRange(arr, mid, arr.length));
+        return merge(left, right);
+    }
+
+    static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+
+
+            k++;
+        }
+//            It may be possible that one of the arrays may not be complete.
+//            Copy all the extra elements in the new array but at the end.
+            while (i < first.length) {
+                mix[k] = first[i];
+                i++;
+                k++;
+            }
+            while (j < second.length) {
+                mix[k] = second[j];
+                j++;
+                k++;
+            }
+            return mix;
+    }
+    static void MergesortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+
+        int mid = (s + e) / 2;
+
+        MergesortInPlace(arr, s, mid);
+        MergesortInPlace(arr, mid, e);
+
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s+l] = mix[l];
+        }
+    }
+}
