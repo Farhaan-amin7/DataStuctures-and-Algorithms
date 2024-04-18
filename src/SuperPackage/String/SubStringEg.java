d8477d697d59dd87eef55e37b91df27f2a218b15
Farhaan Amin Mir <farhaanamin2020@gmail.com>
2024-04-18 14:26:08 +0530

    Subset's added

---
+++ src/SuperPackage/String/SubStringEg.java
@@ -0,0 +1,11 @@
+package SuperPackage.String;
+
+public class SubStringEg {
+    public static void main(String[] args) {
+        StringBuilder name2 = new StringBuilder();
+        String name = "FarhaanAminMir";
+        name2.append(name.substring(0));
+        System.out.println(name2);
+
+    }
+}
